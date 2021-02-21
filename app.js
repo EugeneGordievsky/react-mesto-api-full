9fd6e7ac01c840e9bc070f487242bd5c9c88b6da
Eugene Gordievsky <eugene.gordievsky@yandex.ru>
2021-02-21 23:15:03 +0300

    fix: cors options 9

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(errorLogger);
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('X-Requested-With', 'XMLHttpRequest');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
