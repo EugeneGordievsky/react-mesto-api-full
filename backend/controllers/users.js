84225cef5de85bee9d798b6fcde1139edcb4d7e8
Eugene Gordievsky <eugene.gordievsky@yandex.ru>
2021-02-23 01:40:50 +0300

    fix: token

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -113,7 +113,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.send({
         user,
-        token: token,
+        token,
       });
     })
     .catch(() => next(new AuthError('Ошибка авторизации')));
