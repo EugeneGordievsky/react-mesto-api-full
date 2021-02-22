d6579a215d7badec5f59b057f45598ee7d5a160d
Eugene Gordievsky <eugene.gordievsky@yandex.ru>
2021-02-22 15:43:49 +0300

    fix: cors options 16

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Credentials', 'true');
 
   next();
 });
